40ec91429443c9f6daa4e01b53ac50a75cf5f8bc
shjeng <ghsmsl20@gmail.com>
2024-02-19 11:37:29 +0900

    new project start

---
+++ sharedReview/src/main/java/com/sreview/sharedReview/SharedReviewApplication.java
@@ -0,0 +1,13 @@
+package com.sreview.sharedReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedReviewApplication.class, args);
+	}
+
+}
---
+++ sharedReview/src/test/java/com/sreview/sharedReview/SharedReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreview.sharedReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
