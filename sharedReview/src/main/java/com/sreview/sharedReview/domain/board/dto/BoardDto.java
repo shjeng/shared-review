fdf0ad8e47f83c17b950859e895e0672803c5c2f
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2024-02-24 23:54:28 +0900

    service, dto, entity, controller 작성중

---
+++ sharedReview/src/main/java/com/sreview/sharedReview/domain/board/dto/BoardDto.java
@@ -0,0 +1,20 @@
+package com.sreview.sharedReview.domain.board.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+public class BoardDto {
+    private Long postId;
+    private Long userId;
+//    private UserDto user; // UserDto를 사용하도록 변경
+    private String postTitle;
+    private String postContent;
+    private Long views;
+    private LocalDateTime postCreatedAt;
+    private LocalDateTime postEditDate;
+}
--- sharedReview/src/main/java/com/sreview/sharedReview/domain/controller/BoardController.java
+++ sharedReview/src/main/java/com/sreview/sharedReview/domain/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.sreview.sharedReview.domain.controller;
 
+import com.sreview.sharedReview.domain.board.dto.BoardDto;
 import com.sreview.sharedReview.domain.service.BoardServcice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/board")
@@ -10,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     private final BoardServcice boardServcice;
 
-    @GetMapping("/test")
-    public String test(Dto dto){ // 스트링으로 했음.
-        return boardServcice.save(dto);
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDto boardDto) {
+        boardService.save(boardDto);
+        return "index";
     }
 }
