a8a2deda5644da907eca6dae51651cbc9c54d812
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2024-02-27 17:12:53 +0900

    최신화

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,18 @@
-<<<<<<< HEAD
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Container from './layouts/Container';
-import { MAIN_PATH } from './constant';
->>>>>>> 4885e4d025715ab12e2988cda83fc92025a5e4f8
+import { Route, Routes } from "react-router-dom";
+import Container from "./layouts/Container";
+import { MAIN_PATH } from "./constant";
 
 function App() {
-
-  return ( 
+  return (
     <Routes>
-      <Route element={<Container/>}>
+      <Route element={<Container />}>
         <Route path={MAIN_PATH()} element={<></>} />
       </Route>
     </Routes>
-    )
+  );
 }
 
 export default App;
