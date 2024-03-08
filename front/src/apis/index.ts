6aed36cfe09abf99317d46f42707be5e009793b7
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2024-03-08 22:34:22 +0900

    회원가입 페이지 작성중

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import SignInRequestDto from "./request/auth/sign-in-request.dto";
 import SignInResponseDto from "./response/auth/sign-in.response.dto";
 import ResponseDto from "./response/response.dto";
+import { SignUpRequestDto } from "./request/auth";
+import SignUpResponseDto from "./response/auth/sign-up-response.dto";
 
 const DOMAIN = "http://localhost:8080";
 const API_DOMAIN = `${DOMAIN}/api`;
@@ -15,9 +17,26 @@ const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
 const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
 export const signInRequest = async (requestBody: SignInRequestDto) => {
   // await : 응답이 올 때까지 기다리겠다., requestBody: 어떤 데이터를 넣을 것인지
-  const result = await axios.post(SIGN_IN_URL(), requestBody) // 서버에 post요청
+  const result = await axios
+    .post(SIGN_IN_URL(), requestBody) // 서버에 post요청
     .then((response) => {
       const responseBody: SignInResponseDto = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      if (!error.response.data) return null;
+      const responseBody: ResponseDto = error.response.data;
+      return responseBody;
+    });
+  return result;
+};
+
+// 회원가입 요청
+export const signUpRequest = async (requestBody: SignUpRequestDto) => {
+  const result = await axios
+    .post(SIGN_UP_URL(), requestBody)
+    .then((response) => {
+      const responseBody: SignUpResponseDto = response.data;
       return responseBody;
     })
     .catch((error) => {
