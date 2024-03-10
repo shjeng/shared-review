99eae5b9cf2c14a440049b3681801b9c6c10c934
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2024-03-10 23:07:53 +0900

    로그인 페이지, 회원가입 페이지 분할

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,9 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Container from "./layouts/Container";
-import { AUTH_PATH, MAIN_PATH } from "./constant";
+import { AUTH_PATH, MAIN_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from "./constant";
 import Main from "./views/Main";
 import Authentication from "./views/Authentication";
+import SignIn from "./views/Authentication/sign-in";
+import SignUp from "./views/Authentication/sign-up";
 
 function App() {
   return (
@@ -13,6 +15,10 @@ function App() {
       <Route element={<Container />}>
         <Route path={MAIN_PATH()} element={<Main />} />
         <Route path={AUTH_PATH()} element={<Authentication />} />
+
+        <Route path={SIGN_IN_PATH()} element={<SignIn />} />
+        <Route path={SIGN_UP_PATH()} element={<SignUp />} />
+        {/* <Route path={AUTH_PATH()} element={<Authentication />} /> */}
       </Route>
     </Routes>
   );
--- front/src/constant/index.tsx
+++ front/src/constant/index.tsx
@@ -1,2 +1,5 @@
 export const MAIN_PATH = () => "/";
 export const AUTH_PATH = () => "/auth";
+
+export const SIGN_IN_PATH = () => "/auth/sign-in";
+export const SIGN_UP_PATH = () => "/auth/sign-up";
